f1d579ce472f03b30a4d843db9ff70e70f7f5b85
UrduFictionAdmin <urdufiction.tech@gmail.com>
2023-10-14 23:57:06 +0400

    Added articles API. Dynamic filters pending

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import requests
 
 app = FastAPI()
 
 @app.get("/articles")
 
-def get_articles():
-    return {
-        "articles": [
-            {
-                "title": "Example News Title 1",
-                "content": "Example news content 1.",
-            },
-            {
-                "title": "Example News Title 2",
-                "content": "Example news content 2.",
-            },
-        ]
-    }
+@app.get("/articles")
+async def get_all_articles():
+    try:
+        response = requests.get("https://newsapi.org/v2/everything?q=apple&language=en&from=2023-10-05&sortBy=publishedAt&apiKey=eff0da1b31144e8da7ba3ff9df6876e2")
+        response.raise_for_status()
+        data = response.json()
+        articles = data["articles"]
+        return articles
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=str(e))
