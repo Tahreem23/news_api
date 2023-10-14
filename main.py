e2c93cfe16a4d7f12fbb406f08c8184bb6ab5183
UrduFictionAdmin <urdufiction.tech@gmail.com>
2023-10-15 00:06:08 +0400

    Added dynamic filters

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 import requests
+import datetime
 
 app = FastAPI()
 
-@app.get("/articles")
+API_KEY = "eff0da1b31144e8da7ba3ff9df6876e2"
 
 @app.get("/articles")
-async def get_all_articles():
+async def get_all_articles(query: str = Query(None), language: str = Query("en")):
     try:
-        response = requests.get("https://newsapi.org/v2/everything?q=apple&language=en&from=2023-10-05&sortBy=publishedAt&apiKey=eff0da1b31144e8da7ba3ff9df6876e2")
+        url = f"https://newsapi.org/v2/everything?q={query}&language={language}&from={get_from_date()}&sortBy=publishedAt&apiKey={API_KEY}"
+        response = requests.get(url)
         response.raise_for_status()
         data = response.json()
         articles = data["articles"]
         return articles
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+def get_from_date():
+  today = datetime.date.today()
+  from_date = today - datetime.timedelta(days=7)
+  return from_date.strftime("%Y-%m-%d")
